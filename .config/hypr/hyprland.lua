61cb14633a639a0ec67f2686294769fef9ef51a5
Sain-Biswas <sain.biswas614@gmail.com>
2026-05-19 23:30:14 +0530

    feat: add persistent workspaces

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -22,6 +22,20 @@ hl.monitor({
 })
 
 
+hl.workspace_rule({ workspace = "1", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "2", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "3", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "4", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "5", monitor = "eDP-1", persistent = true })
+
+hl.workspace_rule({ workspace = "6", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "7", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "8", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "9", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "10", monitor = "eDP-1", persistent = true })
+
+
+
 
 
 
