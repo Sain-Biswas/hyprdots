604afda9fe8eb8ae46f261ead96e25c061d26f2b
Sain-Biswas <sain.biswas614@gmail.com>
2026-05-27 12:10:13 +0530

    refactor: enable smart gaps

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -452,41 +452,41 @@ hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl position 5-"), { locked = tr
 -- "Smart gaps" / "No gaps when only"
 -- uncomment all if you wish to use that.
 
--- hl.workspace_rule({
---     workspace = "w[tv1]",
---     gaps_out = 0,
---     gaps_in = 0,
--- })
+hl.workspace_rule({
+    workspace = "w[tv1]",
+    gaps_out = 0,
+    gaps_in = 0,
+})
 
--- hl.workspace_rule({
---     workspace = "f[1]",
---     gaps_out = 0,
---     gaps_in = 0,
--- })
+hl.workspace_rule({
+    workspace = "f[1]",
+    gaps_out = 0,
+    gaps_in = 0,
+})
 
--- hl.window_rule({
---     name        = "no-gaps-wtv1",
+hl.window_rule({
+    name        = "no-gaps-wtv1",
 
---     match       = {
---         float = false,
---         workspace = "w[tv1]",
---     },
+    match       = {
+        float = false,
+        workspace = "w[tv1]",
+    },
 
---     border_size = 0,
---     rounding    = 0,
--- })
+    border_size = 0,
+    rounding    = 0,
+})
 
--- hl.window_rule({
---     name        = "no-gaps-f1",
+hl.window_rule({
+    name        = "no-gaps-f1",
 
---     match       = {
---         float = false,
---         workspace = "f[1]",
---     },
+    match       = {
+        float = false,
+        workspace = "f[1]",
+    },
 
---     border_size = 0,
---     rounding    = 0,
--- })
+    border_size = 0,
+    rounding    = 0,
+})
 
 
 
