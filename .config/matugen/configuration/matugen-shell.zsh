b0a9d2a3975eebb678e02a4127945347e57d38df
Sain-Biswas <sain.biswas614@gmail.com>
2026-08-14 13:34:28 +0530

    feat: change fzf menu style

--- .config/matugen/configuration/matugen-shell.zsh
+++ .config/matugen/configuration/matugen-shell.zsh
@@ -1,5 +1,4 @@
 export FZF_DEFAULT_OPTS=" \
-    --style full \
     --color=fg:{{ colors.on_surface_variant.default.hex }},bg:-1,hl:{{ colors.primary.default.hex }} \
     --color=fg+:{{ colors.on_surface.default.hex }},bg+:{{ colors.surface_container_highest.default.hex }},hl+:{{ colors.on_primary_container.default.hex }} \
     --color=spinner:{{ colors.secondary.default.hex }},header:{{ colors.error.default.hex }},info:{{ colors.tertiary.default.hex }} \
@@ -7,4 +6,3 @@ export FZF_DEFAULT_OPTS=" \
     --color=selected-bg:{{ colors.surface_bright.default.hex }} \
     --color=border:{{ colors.outline.default.hex }},label:{{ colors.primary.default.hex }},gutter:{{ colors.surface.default.hex }} \
     --multi"
-
